e9cf591ed7bf174314c0b70d1fbecbcae5bd43b9
SidselHoegenhaug <201607875@post.au.dk>
2020-02-26 14:31:34 +0100

    Tilføjet coverage

---
+++ ECS.Test.Unit/Fakes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECS.New;
+
+namespace ECS.Test.Unit
+{
+    class MockHeater : IHeater
+    {
+        public bool WasTurnOnCalled { get; set; }
+        public bool WasTurnOffCalled { get; set; }
+
+        public MockHeater()
+        {
+            WasTurnOnCalled = false;
+            WasTurnOffCalled = false;
+        }
+
+        public void TurnOn()
+        {
+            WasTurnOnCalled = true;
+        }
+
+        public void TurnOff()
+        {
+            WasTurnOffCalled = true;
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+
+    class MockTempSensor : ITempSensor
+
+    {
+        public int Temp { get; set; }
+        public bool WasGetTempCalled { get; set; }
+
+        public MockTempSensor()
+        {
+            WasGetTempCalled = true;
+        }
+
+        public int GetTemp()
+        {
+            WasGetTempCalled = true;
+            return Temp;
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
--- ECS.Test.Unit/UnitTest1.cs
+++ ECS.Test.Unit/UnitTest1.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using NSubstitute.Core.DependencyInjection;
 using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace ECS.New
 {
@@ -10,21 +13,43 @@ namespace ECS.New
         private ECS _uut;
         private IHeater _heater;
         private ITempSensor _tempSensor;
-        private int _thr;
+        //private int _thr;
 
         [SetUp]
 
         public void SetUp()
         {
-            _heater = new FakeHeater();
-            _tempSensor = new FakeTempSensor();
-            _thr = 30;
-            _uut = new ECS(_thr, _tempSensor, _heater);
+            _heater = Substitute.For<IHeater>();
+            _tempSensor = Substitute.For<ITempSensor>();
+
+            _uut = new ECS(25, _tempSensor, _heater);
         }
 
         [Test]
-        public void TestMethod1()
+        public void RunSelfTest_TempSensorFails_SelfTestFails()
         {
+            _tempSensor.RunSelfTest().Returns(false);
+            _heater.RunSelfTest().Returns(true);
+            Assert.IsFalse(_uut.RunSelfTest());
+
         }
+
+        [Test]
+
+        public void Regulate_TempBelowThreshold_HeaterTurnedOff()
+        {
+            _tempSensor.GetTemp().Returns(27);
+            _uut.Regulate();
+            _heater.Received(1).TurnOff();
+        }
+
+        [Test]
+        public void Regulate_TempHigherThenThreshold_HeaterTurnedOn()
+        {
+            _tempSensor.GetTemp().Returns(10);
+            _uut.Regulate();
+            _heater.Received(1).TurnOn();
+        }
+
     }
 }
